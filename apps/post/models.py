c1dc2947869f875c3dab6f69263760047ea33f05
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-03-29 18:10:16 +0500

    ready post app

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,6 +1,8 @@
-from django.db.models import ForeignKey, TextField, CASCADE, FileField, Model, DateTimeField
+from django.db.models import ForeignKey, TextField, CASCADE, FileField, Model, DateTimeField, BooleanField, \
+    IntegerField, CharField
 
 from apps.common.models import Base
+from apps.post.choices import NotoificationChoice
 from apps.post.utils import post_upload_path
 
 
@@ -16,7 +18,35 @@ class Post(Base):
     def likes(self):
         return Like.objects.filter(post_id=self.id).count()
 
+
 class Like(Model):
     user = ForeignKey('user.User', on_delete=CASCADE)
     created = DateTimeField(auto_now=True)
     post = ForeignKey('Post', on_delete=CASCADE, related_query_name='likes')
+
+    class Meta:
+        db_table = "likes"
+
+
+class Comment(Model):
+    post = ForeignKey('Post', related_name='comments', on_delete=CASCADE)
+    body = TextField(max_length=500, )
+    is_reply = BooleanField(default=False)
+    author = ForeignKey('user.User', on_delete=CASCADE, related_name='comments')
+    created_at = DateTimeField(auto_now_add=True)
+    like = IntegerField(default=0)
+
+    class Meta:
+        ordering = ['like']
+        db_table = "comments"
+
+
+class Notification(Model):
+    user = ForeignKey('user.User', on_delete=CASCADE)
+    type = CharField(max_length=22, choices=NotoificationChoice.choices)
+    is_seen = BooleanField(default=False)
+    created_at = DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "notfications"
+        ordering = ['is_seen']
--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,3 @@
-
 import os
 from pathlib import Path
 
@@ -9,7 +8,6 @@ env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -22,7 +20,6 @@ DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 DJANGO_APPS = [
     'django.contrib.admin',
@@ -31,7 +28,6 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
 
 ]
 
@@ -78,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -88,7 +83,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -109,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -120,7 +113,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
@@ -137,7 +129,6 @@ LOGIN_REDIRECT_URL = ''
 LOGOUT_REDIRECT_URL = ''
 
 CRISPY_TEMPLATE_PACK = ''
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
