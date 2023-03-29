95148c9dd880ca3e50d0ce74a38bd179deb76fa0
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-03-29 15:57:04 +0500

    start app message

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,10 +1,19 @@
-from django.db import models
+from django.db.models import ForeignKey, TextField, CASCADE, FileField, Model, DateTimeField
 
 from apps.common.models import Base
+from apps.post.utils import post_upload_path
 
 
 class Post(Base):
-
+    author = ForeignKey('user.User', on_delete=CASCADE, related_name="posts")
+    body = TextField(null=True, blank=True)
+    file = FileField(upload_to=post_upload_path, null=False, blank=False)
 
     class Meta:
         db_table = "posts"
+
+
+class Like(Model):
+    user = ForeignKey('user.User', on_delete=CASCADE)
+    created = DateTimeField(auto_now=True)
+    post = ForeignKey('Post', on_delete=CASCADE, related_query_name='likes')
