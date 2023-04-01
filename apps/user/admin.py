f1f7d1be0b34140a1c904d90af45f958daa6eafb
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-04-01 21:19:24 +0500

    change user model admin

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from django.contrib.auth.admin import UserAdmin
 from .models import UserProfile, User
 # Register your models here.
 
 admin.site.register(UserProfile)
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    list_display = ['username', ]
