cb9c011a86beef46f2459c7d3eace62e8373f3e0
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-04-03 12:37:23 +0500

    add static to url

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,9 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 from config import settings
 
 urlpatterns = [
-    path('home/',HomeView.as_view(),name="home" ),
     path('accounts/', include('allauth.urls')),
     path('posts/', include('apps.post.urls')),
     path('home/', include('apps.common.urls')),
@@ -16,5 +13,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
