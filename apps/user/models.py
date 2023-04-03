7162d7b23f0267232e07d4b67b9cccf26eafe3ca
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-04-03 12:32:26 +0500

    moved avater photo to user model from userprofile models

--- apps/user/models.py
+++ apps/user/models.py
@@ -7,15 +7,16 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     followers = models.ManyToManyField('self', related_name='followings', symmetrical=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="profile_picture", null=False, default="default.jpg")
+
     # is_email_verified = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.email
+        return self.username
 
 
 class UserProfile(Base):
     user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="profile_picture", null=True, default="default.jpg")
     first_name = models.CharField(max_length=100, null=True, blank=True)
     last_name = models.CharField(max_length=100, null=True, blank=True)
     bio = models.TextField(max_length=400, null=True, blank=True)
