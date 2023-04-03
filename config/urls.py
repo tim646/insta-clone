368bfa64b67c3e3269103d5db82e2bcef63e5cf5
AzizJohn <azizjon1708@gmail.com>
2023-04-03 12:12:24 +0500

    added static files urls

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -18,3 +19,7 @@ urlpatterns = [
     path('template1', TemplateView.as_view(template_name='messenger.html')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
