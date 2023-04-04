441a432288545d2a16cc229d698135b8802432f5
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-04-04 20:16:38 +0500

    python mime for check file is movie

--- apps/post/forms.py
+++ apps/post/forms.py
@@ -4,8 +4,10 @@ from apps.post.models import Post, Comment
 
 class CommentForm(ModelForm):
     class Meta:
-        model=Comment
-        fields = ['body',]
+        model = Comment
+        fields = ['body', ]
+
+
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
