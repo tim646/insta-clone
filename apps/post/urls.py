b0718ed2a5b162bd5da439e57a28c6451d8ad37b
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-04-07 17:02:25 +0500

    post-create form story order

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 
 from apps.post.views import PostCreateView, UserPostListView, PressLikeView, CommentCreateView, PostDetailView, \
-    PostSaveView
+    PostSaveView, HistoryDetailView
 
 urlpatterns = [
 
+    path('post-create/',PostCreateView.as_view(), name='post-create'),
     path('<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('<str:username>/', UserPostListView.as_view(), name="user-posts"),
     path('like/<int:post_id>',PressLikeView.as_view(), name='press-like'),
     path('history/<int:pk>',HistoryDetailView.as_view(), name='hisrtory-detail'),
     path('saved/<int:post_id>', PostSaveView.as_view(), name='post-save'),
-    path('post/<post_id>/comment',CommentCreateView.as_view(), name='add-comment')
+    path('post/<post_id>/comment',CommentCreateView.as_view(), name='add-comment'),
 ]
