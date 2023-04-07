04e31c9a86eec8b9104a48dc12a3cdd5fd1e9c28
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-04-07 15:57:40 +0500

    register saved from admin page

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from django.contrib.auth.admin import UserAdmin
-from .models import UserProfile, User
+from .models import UserProfile, User, Saved
+
 # Register your models here.
 
 admin.site.register(UserProfile)
 @admin.register(User)
 class UserAdmin(ModelAdmin):
     list_display = ['username', ]
+
+@admin.register(Saved)
+class SavedAdmin(ModelAdmin):
+    list_display = ['user', 'post']
