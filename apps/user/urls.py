f75bcec35733ee6c3ce9a13519125815e2b9c609
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-04-08 17:40:05 +0500

    history add ready

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from .views import SignUpView, CustomLoginView, profile, follow, unfollow, SavedPostView
+from .views import SignUpView, CustomLoginView, profile, follow, unfollow, SavedPostView, UserDetailView
 
 urlpatterns = [
     path('', CustomLoginView.as_view(), name='login'),
     path('mysaved/<username>', SavedPostView.as_view(), name='saved'),
-    # path('profile/<username>/', UserDetailView.as_view(), name='user-profile'),
+    path('profile/<username>/', UserDetailView.as_view(), name='user-profile'),
     path('register/', SignUpView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', profile, name='profile'),
